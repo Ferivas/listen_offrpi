dc1bdb1b6fc720d95adda589b6a7bcd3f656efdb
Ferivas <fvasquez@watching.com.ec>
2020-12-08 12:19:52 -0500

    Añadido retardos para shutdown

--- listen_offrpi.py
+++ listen_offrpi.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import RPi.GPIO as GPIO
 import subprocess
+import time
 
 led_sta=18
 pinoff=22
@@ -12,8 +13,23 @@ GPIO.setup(led_sta, GPIO.OUT)
 #GPIO.output(starpi, True)
 GPIO.output(led_sta, False)
 GPIO.setup(pinoff, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.wait_for_edge(pinoff, GPIO.FALLING)
-print("SLEEP")
-#GPIO.output(starpi, False)
-GPIO.output(led_sta, True)
-subprocess.call(['shutdown', '-h', 'now'], shell=False)
+while True:
+    GPIO.wait_for_edge(pinoff, GPIO.FALLING)
+    print("Ver retardo")
+    counter=0
+    while GPIO.input(pinoff) == False:     
+        counter += 1
+        time.sleep(0.5)
+        tmod=counter%2
+        if tmod==0:
+            GPIO.output(led_sta, False)
+        else:
+            GPIO.output(led_sta, True)
+            
+    if counter>6:
+    #GPIO.output(starpi, False)
+        GPIO.output(led_sta, True)
+        subprocess.call(['shutdown', '-h', 'now'], shell=False)
+    else:
+        print("Retardo insuficiente")
+        GPIO.output(led_sta, False)
