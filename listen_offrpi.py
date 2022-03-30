1f863ac829224dd15e95f776596c0b932bb81e89
Ferivas <fvasquez@watching.com.ec>
2022-03-30 10:06:06 -0500

    Configuración de pines vía archivo json
    Los pines utilizados para el pulsante y el led de status y su polaridad se configuran con un archivo de texto en formato json

--- listen_offrpi.py
+++ listen_offrpi.py
@@ -1,17 +1,58 @@
+'''
+listen_offrpi.py
+Permite apagar de manera segura el Raspberry presionando un pulsante conectado
+entre un GPIO y el GND del Rpi(pinoff)
+Si esta programa esta corriendo tambien se enciende un led coenctado a un GPIO
+del Rpi, (ledsta)
+Es necesario mantener presionado el pulsante durante aproximadamente 3 segundos
+para que se apague el Rpi. Mientras se tiene presionado el pulsante, el ledsta
+titila
+
+'''
 #!/usr/bin/env python
 import RPi.GPIO as GPIO
 import subprocess
 import time
+import json
 
-led_sta=18
-pinoff=22
+
+
+FILECONFIG="listen_offrpi.json"
+print("Comprueba configuracion hw inicial")
+
+confighw=False
+try:
+    with open(FILECONFIG, 'r') as fp:
+        dataconfig = json.load(fp)
+    confighw=True
+except:
+    print("Configuracion default")
+    configdefault={'ledsta':17,
+                   'pinoff':27,
+                   'polaridad':1}
+    with open(FILECONFIG, 'w') as fp:
+        json.dump(configdefault, fp)
+
+
+if not confighw:
+    with open(FILECONFIG, 'r') as fp:
+        dataconfig = json.load(fp)
+    confighw=True
+    
+print(dataconfig)    
+
+
+led_sta=dataconfig.get('ledsta')
+pinoff=dataconfig.get('pinoff')
+polled=dataconfig.get('polaridad')
 #starpi=27
 GPIO.setmode(GPIO.BCM)
 print("Listen for shutdown Rpi")
 GPIO.setup(led_sta, GPIO.OUT)
-#GPIO.setup(starpi, GPIO.OUT)
-#GPIO.output(starpi, True)
-GPIO.output(led_sta, False)
+if polled==0:
+    GPIO.output(led_sta, False)
+else:
+    GPIO.output(led_sta, True)
 GPIO.setup(pinoff, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 while True:
     GPIO.wait_for_edge(pinoff, GPIO.FALLING)
@@ -22,14 +63,26 @@ while True:
         time.sleep(0.5)
         tmod=counter%2
         if tmod==0:
+            if polled==0:
+                GPIO.output(led_sta, False)
+            else:
+                GPIO.output(led_sta, True)
+        else:
+            if polled==0:
+                GPIO.output(led_sta, True)
+            else:
+                GPIO.output(led_sta, False)
+            
+    if counter>6:
+        if polled==0:
             GPIO.output(led_sta, False)
         else:
             GPIO.output(led_sta, True)
-            
-    if counter>6:
-    #GPIO.output(starpi, False)
-        GPIO.output(led_sta, True)
+
         subprocess.call(['shutdown', '-h', 'now'], shell=False)
     else:
         print("Retardo insuficiente")
-        GPIO.output(led_sta, False)
+        if polled==0:
+            GPIO.output(led_sta, False)
+        else:
+            GPIO.output(led_sta, True)
